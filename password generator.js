ee2e04a62b51df22f083734ac0ca42a02700c07b
Rohan <N0989380@my.ntu.ac.uk>
2023-07-26 11:27:57 +0100

    user can see menu and enter in number

--- password generator.js
+++ password generator.js
@@ -11,6 +11,7 @@ let options = {
   includeUppercase: false
 };
 
+//display menu 
 function displayMenu() {
   console.log(`1. [${options.includeSymbols ? 'X' : ' '}] Include Symbols`);
   console.log(`2. [${options.includeNumbers ? 'X' : ' '}] Include Numbers`);
@@ -18,4 +19,20 @@ function displayMenu() {
   console.log(`4. [${options.includeUppercase ? 'X' : ' '}] Include Uppercase Characters`);
 }
 
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+
+//display menu at the start and get users choice
 displayMenu();
+rl.question("Enter your choice (1-4) or press 'q' to quit: ", (input) => {
+  if (input.toLowerCase() === 'q') {
+    console.log("Exiting the password generator.");
+    rl.close();
+  } else {
+    updateMenu(input);
+  }
+});
