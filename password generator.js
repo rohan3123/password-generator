3232de7f5c5166ed757a69754c77bdf43d766c64
Rohan <N0989380@my.ntu.ac.uk>
2023-07-26 11:13:25 +0100

    adding a choice variable and console log for options

--- password generator.js
+++ password generator.js
@@ -2,6 +2,10 @@
 
 function main() {
   console.log("Hello, World! This is a password generator.");
+
+  choice = 0;
+
+  console.log("1.[]Include Symbols\n2.[]Include Numbers\n3.[]Include Lowercase Characters\n4.[]Include Uppercase Characters");
 }
 
 main();
