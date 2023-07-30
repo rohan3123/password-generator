dcbda4284f637adb1aad852e66c23a15f5d2a7b0
Rohan <rohangazi30@gmail.com>
2023-07-30 20:59:52 +0100

    fixed node error

--- password generator.js
+++ password generator.js
@@ -1,37 +1,33 @@
 "use strict";
 
-//list of all possible strings 
+// List of all possible strings
 const symbols = "!@#$%^&*()_-+=[]{}|;:,.<>?";
 const numbers = "0123456789";
 const lowercaseLetters = "abcdefghijklmnopqrstuvwxyz";
 const uppercaseLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
-const possibleChar = [symbols, numbers, lowercaseLetters, uppercaseLetters];
+const possibleCharacters = [symbols, numbers, lowercaseLetters, uppercaseLetters];
 
-//the Password Generation Function
-function Password(length)
-{
-  let password = "";  
+// The Password Generation Function
+function generatePassword(length) {
+  let password = "";
 
-  //for loop to generate a random string based on the arrays 
-  for(let i = 0; i < length; i++ )
-  {
+  // For loop to generate a random string based on the arrays
+  for (let i = 0; i < length; i++) {
     const randomCharacterSet = possibleCharacters[Math.floor(Math.random() * possibleCharacters.length)];
     const randomCharacter = randomCharacterSet[Math.floor(Math.random() * randomCharacterSet.length)];
     password += randomCharacter;
   }
 
-  return password; 
-   
+  return password;
 }
 
-function main()
-{
+function main() {
   // You can change the desired length of the password
-  const passwordLength = 12; 
+  const passwordLength = 12;
 
   const generatedPassword = generatePassword(passwordLength);
   console.log("Generated Password:", generatedPassword);
 }
 
-main()
+main();
